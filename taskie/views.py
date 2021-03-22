e9b3a944685f260d023a7a095db6718001d0f40b
Manav Vagdoda <manav@laptop.local>
2021-03-22 22:59:51 +0530

    item creation succesful

--- taskie/views.py
+++ taskie/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . models import *
+from . forms import *
 
 # Create your views here.
-def index(request):
-    return render(request, 'taskie/tasks.html')
+def defacto(request):
+    tasks = Task.objects.all
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+
+    return render(request, 'taskie/tasks.html', context)
