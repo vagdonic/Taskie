f8bf1cb83b682e453333b2f8a6527d92799a9e43
Manav Vagdoda <manav@laptop.local>
2021-03-23 09:24:26 +0530

    update and delete tasks

--- taskie/urls.py
+++ taskie/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.defacto)
+    path ('', views.defacto, name = "tasks"),
+    path('update<str:pkey>', views.updateTask, name = "update"),
+    path('delete<str:pkey>', views.deleteTask, name = "del"),
 ]
--- taskie/views.py
+++ taskie/views.py
@@ -17,3 +17,24 @@ def defacto(request):
     context = {'tasks': tasks, 'form': form}
 
     return render(request, 'taskie/tasks.html', context)
+
+def updateTask(request, pkey):
+    task = Task.objects.get(id = pkey)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid:
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'taskie/update_task.html', context)
+
+def deleteTask(request, pkey):
+    task = Task.objects.get(id = pkey)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    
+    context = {'task': task}
+    return render(request, 'taskie/del.html', context)
